48a6ed81b44a8df02204ebb53482576ddcd3e71d
Eli Grey <100097497+Eloleespartan@users.noreply.github.com>
2022-03-13 18:54:02 +0000

    if C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = 'a';
+	while(ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return 0;
+}
