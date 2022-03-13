155d3b218cba92e0667860e30e532bb42b338002
Eli Grey <100097497+Eloleespartan@users.noreply.github.com>
2022-03-13 19:20:47 +0000

    if C

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
-	{
-		printf("%d", i);
-	}
-	printf("\n");
-  return 0;
+    for(int i = 0 ; i < 10; i ++)
+    {
+        printf("%d",i);
+    }
+    printf("\n");
+    return(0);
 }
