3bcfb5e4c2da0381ed0d80ff03b88fc846413b5c
Eli Grey <100097497+Eloleespartan@users.noreply.github.com>
2022-03-14 11:19:50 +0000

    if C

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-
 /**
- * main- Entry point
- * 
- * Return: ALways 0 (Success)
- */
- int main(void)
- {
-     char ch ='a';
-     
-     while(ch <='z')
-     {
-         if(ch != 'e' && ch != 'q')
-         {
-             putchar(ch);
-         }
-         ch++;
-     }
-     putchar('\n');
-     return(0);
- }
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void) 
+{
+    for(char ch = 'a'; ch <= 'z'; ch ++)
+    {
+        if(ch != 'e' && ch != 'q')
+        {
+            putchar(ch);
+        }
+    }
+    putchar('\n');
+    return 0;
+}
